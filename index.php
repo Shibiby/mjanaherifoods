e931c9e8f0d612175f6c73fad939d9386d107b58
Muhammad Feisal <37222375+Shibiby@users.noreply.github.com>
2019-05-29 00:39:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'db.php'; ?>
+
+
+<?php require 'header.php'; ?>  
+    <div class="container">
+    <a href="customer_reg.php" class="btn btn-info">Customers Registration</a>
+    </div>
+    <div class="container">
+    <a href="customers.php" class="btn btn-info">Customers</a>
+    </div>
+
+<?php require 'footer.php'; ?>
+
